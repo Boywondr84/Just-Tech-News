7a3717870462739bfac228e9a20359f4d3dac72d
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-18 00:12:21 -0500

    password check and validation added

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,7 +5,7 @@ const { User } = require ('../../models');
 router.get('/', (req, res) => {
     //Access our User model and run .findAll() method
     User.findAll({
-        attributes: { exclude: ['password'] }
+        // attributes: { exclude: ['password'] }
     })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -50,6 +50,29 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    // expect {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: "No user with that email found" });
+            return;
+        } 
+        // res.json({ user: dbUserData });
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: "Incorrect password" });
+            return;
+        } else {
+            res.json({ user: dbUserData, message: "You are now logged in!" });
+        }
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
@@ -57,7 +80,7 @@ router.put('/:id', (req, res) => {
     // if req.body has exact key/value pairs to match the model, you can just use 'req.body' instead
     User.update(req.body, {
         individualHooks: true,
-        
+
         where: {
             id: req.params.id
         }
