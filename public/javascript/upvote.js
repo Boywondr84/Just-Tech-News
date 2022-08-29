b141694aab147a4121f95f673aee2fd7662040bc
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-28 23:33:58 -0500

    fixed upvoting. working properly. TODO fix comment_text. issue with value.trim() in console.log

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -5,6 +5,8 @@ async function upvoteClickHandler(event) {
         window.location.toString().split('/').length - 1
     ];
 
+    console.log(id);
+
     const response = await fetch('/api/posts/upvote', {
         method: 'PUT',
         body: JSON.stringify({
